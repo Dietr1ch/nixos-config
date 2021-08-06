bbfab15c8d28abe5413b74da25f9def0b9cff469
Dietrich Daroch <Dietrich.Daroch@gmail.com>
2021-08-06 12:39:57 -0700

    Security: Add udev rules for TRNG

--- security.nix
+++ security.nix
@@ -1,7 +1,19 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Rules for mounting TrueRNG. https://ubld.it/truerng_v3
+  environment = {
+    systemPackages = with pkgs; [
+      coreutils
+    ];
+  };
+  services.udev.extraRules = ''
+    SUBSYSTEM=="tty", ATTRS{product}=="TrueRNG", SYMLINK+="TrueRNG", RUN+="${pkgs.coreutils}/bin/stty raw -echo -ixoff -F /dev/%k speed 3000000"
+    ATTRS{idVendor}=="04d8", ATTRS{idProduct}=="f5fe", ENV{ID_MM_DEVICE_IGNORE}="1"
+  '';
+
   security = {
+    # Resource limits
     pam.loginLimits = [
       # memlock
       {
